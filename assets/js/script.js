fd3d70e9507c08e247345886e3a82091fcc017ea
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-02-15 17:36:51 -0800

    added function to add class based on time and  get input from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,31 @@ function dateTime() {
     var weekDay = moment().format('dddd, MMMM Do YYYY, HH');
     $('#currentDay').text(weekDay);
 }
+
+dateTime();
+
+// change background color based on time
+function checkTime() {
+    $(textArea).each(function() {
+        if (this.dataset.number < Timer) {
+            $(this).addClass('past');
+        } else if (this.dataset.number === Timer) {
+            $(this).addClass('present');
+        } else if (this.dataset.number > Timer) {
+            $(this).addClass('future');
+        }
+    })
+
+    // get input from local storage
+    $('#09 .form-control').val(localStorage.getItem('09'));
+    $('#10 .form-conrol').val(localStorage.getItem('10'));
+    $('#11 .form-control').val(localStorage.getItem('11'));
+    $('#12 .form-control').val(localStorage.getItem('12'));
+    $('#13 .form-control').val(localStorage.getItem('13'));
+    $("#14 .form-control").val(localStorage.getItem("14"));
+    $("#15 .form-control").val(localStorage.getItem("15"));
+    $("#16 .form-control").val(localStorage.getItem("16"));
+    $("#17 .form-control").val(localStorage.getItem("17"));
+}
+
+checkTime();
